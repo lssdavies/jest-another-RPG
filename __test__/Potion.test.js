823f6e3ecefc44876c11851ed7e1bbadca73495f
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-02-07 16:51:20 -0800

    created potion test and position function to create a new potion

---
+++ __test__/Potion.test.js
@@ -0,0 +1,12 @@
+//Importing  poition
+const Potion = require("../lib/Potion");
+
+/*Test function that takes 2 args i.e a descriptive string of the expected result and a call back arrow function*/
+test("creates a random potion object", () => {
+  const potion = new Potion();
+
+  //we chain toEqual with a toBeGreater than to test any string or number for potion name and potion value. the toBeGreater is checking that potion is not blank using name.length since if there is a name length will be greater than 0.
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
+  expect(potion.value).toEqual(expect.any(Number));
+});
