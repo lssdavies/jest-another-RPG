8f69c620d528f1280c8b9a2f35ea1ebac88a7621
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-02-07 21:12:38 -0800

    updated player.js with method for getStats and getInventory methods using prototypes as opposed to this. syntax

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -6,6 +6,7 @@ jest.mock("../lib/Potion");
 console.log(new Potion());
 
 test("creates a player object", () => {
+  //The new keyword creates an empty object using the constructor function Player() and assigns it to this and then returns this
   const player = new Player("Dave");
 
   expect(player.name).toBe("Dave");
@@ -16,4 +17,24 @@ test("creates a player object", () => {
   expect(player.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
   );
+});
+/*the getStats() method will return an object containing a subset of the player's properties. */
+test("gets player's stats as an object", () => {
+  const player = new Player("Dave");
+  //we're checking that player.getStats() returns an object with four specific properties.
+  expect(player.getStats()).toHaveProperty("potions");
+  expect(player.getStats()).toHaveProperty("health");
+  expect(player.getStats()).toHaveProperty("strength");
+  expect(player.getStats()).toHaveProperty("agility");
+});
+
+/*the getInventory() method will return an array of Potion objects or return false if the inventory is empty.*/
+test("gets inventory from player or returns false", () => {
+  const player = new Player("Dave");
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
 });
--- __test__/Potion.test.js
+++ __test__/Potion.test.js
@@ -3,6 +3,7 @@ const Potion = require("../lib/Potion");
 
 /*Test function that takes 2 args i.e a descriptive string of the expected result and a call back arrow function*/
 test("creates a random potion object", () => {
+  //The new keyword creates an empty object using the constructor function Potion() and assigns it to this and then returns this
   const potion = new Potion();
 
   //we chain toEqual with a toBeGreater than to test any string or number for potion name and potion value. the toBeGreater is checking that potion is not blank using name.length since if there is a name length will be greater than 0.
